57bd46a1bb1f4aa66e16ca9a0ea1712eaa9b6460
Jordan Blake <jordan.blake@outlook.com>
2016-12-06 13:35:14 +0000

    Fixes rotation for SpriteRenderable
    More specifically, fixes rotation around an origin.

--- src/insolence/system/sprite_renderable_system.cpp
+++ src/insolence/system/sprite_renderable_system.cpp
@@ -16,16 +16,17 @@ void SpriteRenderableSystem::Manage(const GameTime& gametime)
 		if(s->hidden == true || m == NULL || s->texture == 0)
 			continue;
 
+		glm::mat4 model;
+
 		s->animation.Progress(gametime.GetFrameTime());
 
-		glm::mat4 model = m->GetModelMatrix();
-
-		model = glm::translate(model,
-				glm::vec3(s->origin.x, s->origin.y, 0.f));
-		model = glm::rotate(model, glm::radians(360 - s->rot),
-				glm::vec3(0.f, 0.f, 1.f));
+		model = m->GetModelMatrix();
 		model = glm::translate(model,
 				glm::vec3(-s->origin.x, -s->origin.y, 0.f));
+		model = glm::rotate(m->GetModelMatrix(), glm::radians(360 - s->rot),
+				glm::vec3(0.f, 0.f, 1.f));
+		model = glm::translate(model,
+				glm::vec3(s->origin.x, s->origin.y, 0.f));
 
 		renderer->Add(s->texture, model, s->colour,
 				s->animation.GetAnimationRectangle(s->texture), s->layer);
