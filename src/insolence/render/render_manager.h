dce1b6c3ea3e903f9c9de932abf9004813c8e78f
Jordan Blake <jordan.blake@outlook.com>
2017-02-15 22:42:50 +0000

    Changes RenderManager CreatePair error checking
    If we're unsuccessful in creating the shader program, return NULL. Also
    moves the function to the cpp file.

--- src/insolence/render/render_manager.h
+++ src/insolence/render/render_manager.h
@@ -18,58 +18,13 @@ struct INSOLENCE_API RenderManager
 	GLint uni_projection;
 	GLint uni_view;
 
-	/**
-	 * The renderer drawing at the end of the frame.
-	 */
-	virtual void Flush(const GameTime &gametime)=0;
-
 	RenderManager() {}
 	virtual ~RenderManager();
 
-	/**
-	 * Creates a shader program from a vertex/frag pair.
-	 *
-	 * \param vert_path	Filepath of vertex shader.
-	 * \param frag_path	Filepath of fragment shader.
-	 */
+	virtual void Flush(const GameTime &gametime)=0;
+
 	static ShaderProgram* CreateShaderProgramFromPair(const char *vert_path,
-			const char *frag_path)
-	{
-		Shader **shaders = new Shader*[2];
-		ShaderProgram *program = new ShaderProgram();
-
-		shaders[0] = Shader::CreateFromFile(vert_path, GL_VERTEX_SHADER,
-				ShaderFlags::ADD_HEADER);
-		shaders[1] = Shader::CreateFromFile(frag_path, GL_FRAGMENT_SHADER,
-				ShaderFlags::ADD_HEADER);
-
-		for(int i = 0; i < 2; ++i)
-		{
-			if(shaders[i]->GetCompileStatus() != GL_TRUE)
-			{
-				shaders[i]->LogCompileInfo("RenderManager (%s) - "
-						"%s ", __FUNCTION__, i == 0 ? "Vert" : "Frag");
-			}
-			else
-				glAttachShader(program->GetID(), shaders[i]->GetID());
-		}
-
-#ifdef INSOLENCE_OPENGL_DESKTOP
-		glBindFragDataLocation(program->GetID(), 0, "out_colour");
-#endif
-		glLinkProgram(program->GetID());
-		glUseProgram(program->GetID());
-
-		for(int i = 0; i < 2; ++i)
-			delete shaders[i];
-
-		delete[] shaders;
-
-		log(Log::INFO, "RenderManager (%s) - Successfully created pair",
-				__FUNCTION__);
-
-		return program;
-	}
+			const char *frag_path);
 };
 
 #endif
